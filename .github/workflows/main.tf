d8b399797b0a5f68859ef6daac4ae79f33df98a4
solunwa <olunwasundayc@gmail.com>
2023-10-07 08:47:05 -0700

    mod

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,5 +1,10 @@
-provider "aws" {}
+provider "aws" {
+    region = "us-east-2"
+}
 
 resource "aws_vpc" "test" {
     cidr_block = "10.0.0.0/16"
+    tags = {
+        Name ="Actions-test"
+    }
 }
